4ea6ca0ad1514d8b8a65398eeb068dc927a9cc26
Andrew Thomas <andrewhthomas@gmail.com>
2017-08-26 14:07:07 -0400

    Starting on about page

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,8 +1,13 @@
 <template>
-  <nav class="bg-green pa3 pa4-ns">
-  <a class="link dim black b f6 f5-ns dib mr3" href="#" title="Home">Site Name</a>
-  <a class="link dim white    f6 f5-ns dib mr3" href="#" title="About">About</a>
-  <a class="link dim white    f6 f5-ns dib mr3" href="#" title="Store">Store</a>
-  <a class="link dim white    f6 f5-ns dib" href="#" title="Contact">Contact</a>
+  <nav class="bg-green db dt-l w-100 border-box pa3 pa5-ns ph5-l">
+  <nuxt-link class="db dtc-l v-mid white fw9 link dim w-100 w-25-l tc tl-l mb2 mb0-l" to="/" title="Home">
+    A. Thomas</nuxt-link>
+  <div class="db dtc-l v-mid w-100 w-75-l tc tr-l">
+    <nuxt-link class="link dim white f6 fw1 tracked f5-l dib mr3 mr4-l" to="/about" title="About">About</nuxt-link>
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="How it Works">How it Works</a>
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Blog">Blog</a>
+    <a class="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Press">Press</a>
+    <a class="link dim dark-gray f6 f5-l dib" href="#" title="Contact">Contact</a>
+  </div>
 </nav>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 <navigation/>
 <div class="bg-green avenir pa3 pa5-m pa5-l pa5-ns mb3 cf bb b--black-05">
   <div class="fl w-100 w-100-m w-60-l w-60-ns">
-    <h1 class="f3 f2-m f2-l f2-ns black-70 mt0 pt3">Andrew H. Thomas</h1>
+    <h1 class="f3 f2-m f2-l f2-ns black-70 mt0 pt3">Front-end Developer, Drupal &amp; WordPress</h1>
     <p class="f4 f3-m f3-l f3-ns black-90 measure lh-copy">
       I build websites with Drupal, WordPress, Grav, and a variety of other great open source software. I design HTML5 themes and templates with Sass and Less, and use Gulp as my task runner.
 </p>
@@ -13,9 +13,6 @@ Right now I'm focusing on learning React and brushing up on my Laravel skills.
 </p>
 <p class="f4 f3-m f3-l f3-ns black-90 measure lh-copy">I currently work for the publishing services firm Scribe Inc.</p>
     </p>
-  </div>
-  <div class="fl w-40 center tc dn dn-m db-l db-ns">
-    <img data-src="images/donuts2.svg" class=" lazyloaded" src="images/donuts2.svg">
   </div>
 </div>
 </div>
